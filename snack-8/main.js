8c70842a4a9869c77d021b22f4c527992321c602
Greg1928 <gregoriosortino@protonmail.com>
2022-05-04 17:35:30 +0200

    snack 8

---
+++ snack-8/main.js
@@ -0,0 +1,15 @@
+const x = [];
+const dispari = [];
+let somma = 0;
+for ( let i = 0; i<4; i++){
+    x.push(Number(prompt("inserisci numero")));
+}
+console.log(x);
+for ( let i = 0; i<4; i++){
+    if(x[i]%2 != 0){
+        dispari.push(x[i]);
+        somma += x[i];
+    }
+}
+console.log(dispari);
+console.log(somma);
