248397a75db8fa8ba091aca44bf19eaf2c416f51
Greg1928 <gregoriosortino@protonmail.com>
2022-05-04 11:45:28 +0200

    snack 2

---
+++ snack-2/main.js
@@ -0,0 +1,10 @@
+let x = prompt("Inserisci prima parola");
+let y = prompt("Inserisci seconda parola");
+
+if( x.length > y.length){
+    console.log(y);
+    console.log(x);
+}else {
+    console.log(x);
+    console.log(y);
+}
