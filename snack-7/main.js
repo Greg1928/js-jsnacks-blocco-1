9b04da02638f3ddef3c7e84005249707fa449275
Greg1928 <gregoriosortino@protonmail.com>
2022-05-04 16:53:19 +0200

    snack 7

---
+++ snack-7/main.js
@@ -0,0 +1,7 @@
+const nomi = ["marco", "nikolas", "samuele"];
+const cognomi = ["acciari", "mosca", "madrigali"];
+
+console.log(nomi[Math.floor(Math.random() * 3)]);
+console.log(cognomi[Math.floor(Math.random() * 3)]);
+
+
